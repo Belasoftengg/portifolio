cfe8892fc2e2b4f719f8872601ed13caf268ef62
Belasoftengg <155488654+Belasoftengg@users.noreply.github.com>
2025-08-07 21:59:05 +0300

    aewsrdtygu

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,15 +1,12 @@
 // Import images
-import WebImage1 from '../images/web-project-1.jpg';
-import WebImage2 from '../images/web-project-2.jpg';
-import MobileImage1 from '../images/mobile-project-1.jpg';
-import MobileImage2 from '../images/mobile-project-2.jpg';
-import UIImage1 from '../images/ui-project-1.jpg';
-import UIImage2 from '../images/ui-project-2.jpg';
+import WebImage2 from '../images/Tourist Hotel.jpeg';
+import UIImage1 from '../images/Tourist Hotel.jpeg';
+import UIImage2 from '../images/Haile Resort.jpeg';
 
 export const projectsData = [
 	{
 		id: 1,
-		title: 'Google Health Platform',
+		title: 'Website for Volunters Organization',
 		category: 'Web Application',
 		img: WebImage2,
 		ProjectHeader: {
@@ -18,34 +15,18 @@ export const projectsData = [
 			tags: 'UI / Frontend',
 		},
 	},
+	
 	{
 		id: 2,
-		title: 'Phoenix Digital Agency',
-		category: 'Mobile Application',
-		img: MobileImage2,
-	},
-	{
-		id: 3,
-		title: 'Project Management UI',
+		title: 'Arbaminch Tourist Hotel',
 		category: 'UI/UX Design',
 		img: UIImage1,
 	},
 	{
-		id: 4,
-		title: 'Cloud Storage Platform',
-		category: 'UI/UX Design',
-		img: UIImage2,
-	},
-	{
-		id: 5,
-		title: 'React Social App',
-		category: 'Mobile Application',
-		img: MobileImage1,
-	},
-	{
-		id: 6,
-		title: 'Apple Design System',
+		id: 3,
+		title: 'Haile Resort Arbaminch',
 		category: 'Web Application',
-		img: WebImage1,
-	},
+		img: UIImage2,
+	}
+
 ];
