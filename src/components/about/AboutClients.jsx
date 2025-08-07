a3b974b4aa74b2c8639a4ad7120393d42b889d70
Belasoftengg <155488654+Belasoftengg@users.noreply.github.com>
2025-08-07 21:01:50 +0300

    dsadjhg

--- src/components/about/AboutClients.jsx
+++ src/components/about/AboutClients.jsx
@@ -4,7 +4,6 @@ import AboutClientSingle from './AboutClientSingle';
 
 const AboutClients = () => {
 	const { clientsData, clientsHeading } = useContext(AboutMeContext);
-
 	return (
 		<div className="mt-10 sm:mt-20">
 			<p className="font-general-medium text-2xl sm:text-3xl  text-center text-primary-dark dark:text-primary-light">
