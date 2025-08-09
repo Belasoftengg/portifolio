edd4e99d6b6fd26fcbdb628e9a2531b6d43a0d9b
Belasoftengg <155488654+Belasoftengg@users.noreply.github.com>
2025-08-09 15:27:04 +0300

    dsndnasm,\

--- src/components/about/AboutMeBio.jsx
+++ src/components/about/AboutMeBio.jsx
@@ -1,4 +1,4 @@
-import profileImage from '../../images/save.jpg';
+import profileImage from '../../images/belay.jpg';
 import { useContext } from 'react';
 import AboutMeContext from '../../context/AboutMeContext';
 
